5d0697d4f8fcd3fe6b558aa804eb005ad7c8644d
Aleare <atarax97@gmail.com>
2023-12-18 17:27:51 +0100

    Fixed repo

--- test_constrained.jl
+++ test_constrained.jl
@@ -11,17 +11,3 @@ d = 2 # set the relaxation order
 #numeq=number of equations
 #term sparsity: idk
 opt,sol,data = tssos_first(pop, x, d, numeq=1, TS="MD")
-=======
-using TSSOS
-using DynamicPolynomials
-
-@polyvar x[1:3] #define x1 x2 x3 as poly opt vars
-f = 1+x[1]^4+x[2]^4+x[3]^4+x[1]*x[2]*x[3]+x[2] #cost function 
-g_1 = 1-x[1]^2-2*x[2]^2 #uneq constraint
-g_2 = x[2]^2+x[3]^2-1 #eq constraint
-pop = [f, g_1, g_2]
-d = 2 # set the relaxation order
-#numeq=number of equations
-#term sparsity: idk
-opt,sol,data = tssos_first(pop, x, d, numeq=1, TS="MD")
->>>>>>> 036cf96c2e13a58881bca4537ae268f7a91d29ce
--- test_sysid.jl
+++ test_sysid.jl
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using TSSOS
 using DynamicPolynomials
 using MAT
@@ -40,46 +39,3 @@ opt,sol,data = tssos_first(pop, [theta;e;eta], d, numeq=length(g), TS="MD", quot
 println(opt)
 println(sol)
 println(data)
-=======
-using TSSOS
-using DynamicPolynomials
-using MAT
-
-theta0=[-0.7647,0.3012,0,32.24,21.41];
-println(theta0)
-println("Obtaining data from matlab")
-
-vars = matread("matfile.mat");
-varnames = keys(vars);
-print("Got: ")
-println(varnames)
-delta_e=vars["Delta_epsilon"]
-delta_eta=vars["Delta_eta"]
-u_tilde=vars["u_tilde"];
-y=vars["y"];
-
-@polyvar theta[1:5];
-@polyvar e[1:100];
-@polyvar eta[1:100];
-
-start=3;
-Ni=30;
-
-#test: PUI_1 min
-f=theta[1]
-g=[];
-for i=start:Ni
-    push!(g, y[i] + theta[1]*y[i-1] + theta[2]*y[i-2] +
-     theta[3]*u_tilde[i] + theta[4]*u_tilde[i-1] + theta[5]*u_tilde[i-2] -
-     eta[i]+ theta[1]*eta[i-1] + theta[2]*eta[i-2] +
-     theta[3]*e[i] + theta[4]*e[i-1] + theta[5]*e[i-2])
-end
-pop = g;
-pushfirst!(pop,f);
-d = 2; # set the relaxation order
-println("Startting POP via TSSOS")
-opt,sol,data = tssos_first(pop, [theta;e;eta], d, numeq=length(g), TS="MD", quotient=false)
-println(opt)
-println(sol)
-println(data)
->>>>>>> 036cf96c2e13a58881bca4537ae268f7a91d29ce
