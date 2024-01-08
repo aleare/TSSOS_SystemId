f2740c2970361e0cadfae1438538ec2b6c00579d
Aleare <atarax97@gmail.com>
2024-01-08 13:03:21 +0100

    TSSOS starts

--- test_sysid.jl
+++ test_sysid.jl
@@ -1,31 +1,48 @@
 using TSSOS
 using DynamicPolynomials
 using MAT
+using Printf
 
 theta0=[-0.7647,0.3012,0,32.24,21.41];
 println(theta0)
 println("Obtaining data from matlab")
 
-vars = matread("matfile.mat");
+vars = matread("test2.mat");
 varnames = keys(vars);
 print("Got: ")
 println(varnames)
-delta_e=vars["Delta_epsilon"]
+N=convert(Int64, vars["N"])
+@printf("N: %d\n",N)
+delta_epsilon=vars["Delta_epsilon"]
+@printf("Delta Epsilon: %f\n",delta_epsilon)
 delta_eta=vars["Delta_eta"]
+@printf("Delta Eta: %f\n",delta_eta)
 u_tilde=vars["u_tilde"];
 y=vars["y"];
 
+N=20
+
 @polyvar theta[1:5];
-@polyvar e[1:100];
-@polyvar eta[1:100];
+@polyvar e[1:N];
+@polyvar eta[1:N];
 
 start=3;
-Ni=30;
 
 #test: PUI_1 min
 f=theta[1]
 g=[];
-for i=start:Ni
+
+for i=1:N
+    if(delta_eta!=0)
+        push!(g,eta[i]+delta_eta);
+        push!(g,-eta[i]+delta_eta);
+    end
+    if(delta_epsilon!=0)
+        push!(g,e[i]+delta_epsilon);
+        push!(g,-e[i]+delta_epsilon);
+    end
+end
+for i=start:N
     push!(g, y[i] + theta[1]*y[i-1] + theta[2]*y[i-2] +
      theta[3]*u_tilde[i] + theta[4]*u_tilde[i-1] + theta[5]*u_tilde[i-2] -
      eta[i]+ theta[1]*eta[i-1] + theta[2]*eta[i-2] +
@@ -33,9 +50,18 @@ for i=start:Ni
 end
 pop = g;
 pushfirst!(pop,f);
+@printf("\n\n\n")
+#debug
+print(length(pop))
+print("Numeq:")
+println(N-start+1)
+println("-")
+println(pop)
+println("-")
+println( [theta;e;eta])
 d = 2; # set the relaxation order
-println("Startting POP via TSSOS")
-opt,sol,data = tssos_first(pop, [theta;e;eta], d, numeq=length(g), TS="MD", quotient=false)
+println("Starting POP via TSSOS")
+opt,sol,data = tssos_first(pop, [theta;e;eta], d, numeq=N-start+1, TS="MD", solution=true, quotient=false)
 println(opt)
 println(sol)
 println(data)
